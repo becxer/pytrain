36b12cda5b348988a1f0d8ba67f247d57b7129ef
becxer <becxer87@gmail.com>
2016-05-08 22:46:55 +0900

    fix HierarchicalClustering bug

--- pytrain/HierarchicalClustering/HierarchicalClustering.py
+++ pytrain/HierarchicalClustering/HierarchicalClustering.py
@@ -19,24 +19,26 @@ class HierarchicalClustering:
         self.col_len = len(self.mat_data[0])
         self.row_len = len(self.mat_data)
         self.unique_idx = 0
+        self.group_list = []
+        self.dist_list = []
+        self.cluster_points = []
         
     def fit(self):
         return self.cluster()
 
     unique_idx = 0
-    group_list = []
-    dist_list = []
     group_map = {}
 
     class Group:
-        data_idx = []  # original data index
-        vector = [] # group's vector
         def __init__(self, vt, didx):
-            HierarchicalClustering.unique_idx += 1
             self.unique_idx = HierarchicalClustering.unique_idx
             self.vector = vt
-            self.data_idx.extend(didx)
+            self.data_idx = didx
             HierarchicalClustering.group_map[self.unique_idx] = self
+            HierarchicalClustering.unique_idx += 1
+
+        def log(self):
+            print "[ Group", self.unique_idx, "] vt : ", self.vector, ", data_idx : ", str(self.data_idx)
 
     class Dist:
         def __init__(self, s_grp, t_grp, dist_func):
@@ -44,35 +46,33 @@ class HierarchicalClustering:
             self.trg_idx = t_grp.unique_idx
             self.distance = dist_func(s_grp.vector, t_grp.vector)
 
+        def log(self):
+            print "[ Dist ]", self.src_idx, "-", self.trg_idx, " = ", self.distance
+
     def remove_from_dist_list(self, grp_idx):
-        for dist_obj in self.dist_list:
-            if dist_obj.src_idx == grp_idx or \
-              dist_obj.trg_idx == grp_idx:
-              self.dist_list.remove(dist_obj)
+        self.dist_list = [dist_obj for dist_obj in self.dist_list \
+            if (dist_obj.src_idx != grp_idx and dist_obj.trg_idx != grp_idx)]
 
     def remove_from_group_list(self, grp_idx):
-        for grp in self.group_list:
-            if grp.unique_idx == grp_idx:
-                self.group_list.remove(grp)
-                break
+        self.group_list = [grp_obj for grp_obj in self.group_list \
+            if (grp_obj.unique_idx != grp_idx) ]
         
     def insert_new_group(self, grp):
         for oth in self.group_list:
             new_dis = self.Dist(grp, oth, self.dist_func)
             for idx, old_dis in enumerate(self.dist_list):
-                if new_dis.distance <= old_dis.distance:
+                if new_dis.distance >= old_dis.distance:
                     self.dist_list.insert(idx, new_dis)
                     break
-                    
         self.group_list.append(grp)
     
     def merge_group(self, grp_1_idx, grp_2_idx):
         grp_1 = self.group_map[grp_1_idx]
         grp_2 = self.group_map[grp_2_idx]
-        
+
         mgd_vt = ( (grp_1.vector * len(grp_1.data_idx)) \
-                       + (grp_2.vector + len(grp_2.data_idx)) ) \
-                           / (len(grp_1.data_idx) + len(grp_2.data_idx))
+                       + (grp_2.vector * len(grp_2.data_idx)) ) \
+                           / ( len(grp_1.data_idx) + len(grp_2.data_idx))
         mgd_didx = []
         mgd_didx.extend(grp_1.data_idx)
         mgd_didx.extend(grp_2.data_idx)
@@ -81,35 +81,36 @@ class HierarchicalClustering:
     
     def cluster(self):
         # make initial groups
-        self.group_list = [ self.Group(vt,[idx]) for idx, vt in enumerate(self.mat_data)]
+        for idx, vt in enumerate(self.mat_data):
+            self.group_list.append(self.Group(vt, [idx]))
+
         # make dist_list
         for i, src_g in enumerate(self.group_list):
             for j in range(i+1,len(self.group_list)):
                 trg_g = self.group_list[j]
                 self.dist_list.append(self.Dist(src_g, trg_g, self.dist_func))
-
+                
         # merge group until length of group list less than K
-        self.dist_list.sort(key=lambda x : x.distance,reverse=False)
+        self.dist_list.sort(key=lambda x : x.distance,reverse = True)
+
         while len(self.group_list) > self.K :
             selected_dist = self.dist_list.pop()
             new_group = self.merge_group(selected_dist.src_idx, selected_dist.trg_idx)
-
             self.remove_from_dist_list(selected_dist.src_idx)
             self.remove_from_dist_list(selected_dist.trg_idx)
             self.remove_from_group_list(selected_dist.src_idx)
             self.remove_from_group_list(selected_dist.trg_idx)
-        
             self.insert_new_group(new_group)
             
         # loop group list & fill label data
         self.label_data = [-1 for x in range(len(self.mat_data))]
-        for grp in self.group_list:
+        for grp_idx, grp in enumerate(self.group_list):
+            self.cluster_points.append(grp.vector)
             for idx in grp.data_idx:
-                self.label_data[idx] = grp.unique_idx
+                self.label_data[idx] = grp_idx
         return self.label_data
 
     # default predictor using KNN
     def predict(self, input_array):
-        # TODO
-        input_array = convert.list2npfloat(input_array)
+        print self.cluster_points
         pass
